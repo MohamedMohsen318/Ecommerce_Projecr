0e029c8ffff24574d72b26b9555110df652ee538
Mohamed Mohsen <Mohamedsn101@gmail.com>
2026-06-22 16:55:18 +0300

    Redesign welcome store page

--- app/Enums/CartStatus.php
+++ app/Enums/CartStatus.php
@@ -24,6 +24,8 @@ enum CartStatus: string
 
     public static function values(): array
     {
-        return array_column(self::cases(), 'value');
+        return collect(self::cases())
+            ->pluck('value')
+            ->all();
     }
 }
